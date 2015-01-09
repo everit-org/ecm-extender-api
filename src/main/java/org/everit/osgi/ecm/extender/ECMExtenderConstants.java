85e8256a9b894c6106564c61a60b89573bbe3741
Balazs Zsoldos <balazs.zsoldos@everit.biz>
2015-01-09 15:19:35 +0100

    Refactoring package name

--- src/main/java/org/everit/osgi/ecm/extender/ECMExtenderConstants.java
+++ src/main/java/org/everit/osgi/ecm/extender/ECMExtenderConstants.java
@@ -14,7 +14,7 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with Everit - ECM Extender API.  If not, see <http://www.gnu.org/licenses/>.
  */
-package org.everit.osgi.ecm.extender.api;
+package org.everit.osgi.ecm.extender;
 
 /**
  * Constant values of ECM Extender.
